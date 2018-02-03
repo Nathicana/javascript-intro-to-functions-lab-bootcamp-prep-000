d5d4dd9a7b45c11094ddb307fcae7d145a40ce77
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-02-03 05:34:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,14 @@ function whisper(string){
 }
 whisper("HELLO!");
 
+function logWhisper(string){ 
+ console.log(string.toLowerCase()); 
+ logWhisper("HELLO!");
+ }
 
+
+function logShout(string){ 
+ console.log(string.toUpperCase()) }
+ logShout("hello!");
+ 
  
